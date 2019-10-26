5a4fc6d5269806eec18923e66f442b7beeb09402
Craig Blackburn <cr.blackburn89@gmail.com>
2019-10-26 14:32:56 -0600

    Do some setup/config stuff

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,18 @@
 <template>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        goal-tracker
-      </h1>
+      <h1 class="title">Goal Tracker</h1>
       <h2 class="subtitle">
         Goal tracking app based on the PACE Performance Strategy
       </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
   components: {
-    Logo
+    //
   }
 }
 </script>
@@ -60,9 +43,5 @@ export default {
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
